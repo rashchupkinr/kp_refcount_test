ffdae01ebdf70c2771b6588bb3904815e5ed1e34
Roman Rashchupkin <14759822+rashchupkinr@users.noreply.github.com>
2024-06-02 11:28:33 +0200

    Run iterations of refcount_test every 100ms
    - initialize testing refcounter statically
    
    Signed-off-by: Roman Rashchupkin <14759822+rashchupkinr@users.noreply.github.com>

--- kp_refcount_test.c
+++ kp_refcount_test.c
@@ -5,58 +5,56 @@
 #include <linux/types.h>
 #include <linux/printk.h>
 #include <linux/refcount.h>
+#include "kp_refcount_test.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("kp_refcount_test");
 MODULE_VERSION("0.8");
 
-refcount_t count;
+refcount_t test_refcount = REFCOUNT_INIT(1);
+EXPORT_SYMBOL(test_refcount);
 
 int refcount_test_iter(void)
 {
 	int i;
-	for (i=0; i<NREF; i++) {
-		refcount_inc(&count);
-	}
-	for (i=0; i<NREF; i++) {
-		if (refcount_dec_and_test(&count))
+	for (i=0; i<NREF; i++)
+		refcount_inc(&test_refcount);
+	for (i=0; i<NREF; i++)
+		if (refcount_dec_and_test(&test_refcount))
 			return -1;
-	}
 	return 0;
 }
 
 int refcount_test(void)
 {
 	int i;
-	refcount_set(&count, 1);
 	for (i=0; i<1000; i++)
 		if (refcount_test_iter())
 			return -1;
-	if (refcount_read(&count) == 1)
-		return 0;
-	return -1;
+	return 0;
 }
 EXPORT_SYMBOL(refcount_test);
 
+struct delayed_work kp_test_work;
+
 void start_refcount_test(void)
 {
-	if (refcount_test())
-		printk(KERN_ERR "refcount_test: error\n");
-	else
-		printk(KERN_ERR "refcount_test: success\n");
+	if (refcount_test()) {
+		pr_warn("refcount_test: error\n");
+		cancel_delayed_work_sync(&kp_test_work);
+	}
 }
 
-struct delayed_work kp_test_work;
 void kp_refcount_test_work(struct work_struct *work)
 {
 	start_refcount_test();
-	schedule_delayed_work(&kp_test_work, msecs_to_jiffies(1000));
+	schedule_delayed_work(&kp_test_work, msecs_to_jiffies(100));
 }
 
 static __init int kp_refcount_test_init(void)
 {
 	INIT_DELAYED_WORK(&kp_test_work, kp_refcount_test_work);
-	schedule_delayed_work(&kp_test_work, msecs_to_jiffies(1000));
+	schedule_delayed_work(&kp_test_work, 0);
 	return 0;
 }
 
